12ee93129e1e53caa9588e84e1b6bc80fbe76300
Tony Nebraska <example@gmail.com>
2022-12-26 18:06:58 +0300

    second one

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+    });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+  });
